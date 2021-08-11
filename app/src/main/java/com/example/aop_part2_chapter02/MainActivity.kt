0d216e354d1565392849454f9de0a9c63849d183
Sohee <sohee3480@gmail.com>
2021-08-11 17:24:42 +0900

    버튼을 변수로 만들어주기

--- app/src/main/java/com/example/aop_part2_chapter02/MainActivity.kt
+++ app/src/main/java/com/example/aop_part2_chapter02/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.aop_part2_chapter02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.NumberPicker
 
 class MainActivity : AppCompatActivity() {
+
+    private val clearBtn: Button by lazy {
+        findViewById(R.id.clearBtn)
+    }
+
+    private val addBtn: Button by lazy {
+        findViewById(R.id.addBtn)
+    }
+
+    private val runBtn: Button by lazy {
+        findViewById(R.id.runBtn)
+    }
+
+    private val numberPicker: NumberPicker by lazy {
+        findViewById(R.id.numberPicker)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 }
